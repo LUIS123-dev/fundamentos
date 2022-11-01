ee395b9fb83805de87bc1f4ee29cc88d6b6c54ab
Luis <luisfernandosantosbezerr@gmail.com>
2022-11-01 16:28:03 -0300

    objetos

---
+++ 02_cursoJavaScript/01_objetos.js
@@ -0,0 +1,17 @@
+function book(title, author, pages, read, info) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = info
+    this.description = function () {
+        return info
+    }
+}
+
+const theHobbit = new book('The Hobbit', 'John Ronald Reuel Tolkien', 500, false,
+    'The Hobbit, or There and Back Again é um livro infantojuvenil de alta fantasia escrito pelo filólogo e professor britânico J. R. R. Tolkien')
+
+console.log(theHobbit)
+
+console.log(book.prototype)
