ca155a638ee1558730805e0a10ff766cc5020ba6
Luis <luisfernandosantosbezerr@gmail.com>
2022-11-03 16:16:44 -0300

    jogo da velha

--- 02_cursoJavaScript/01Até08/08_modulos.js
+++ 02_cursoJavaScript/01Até08/08_modulos.js
@@ -6,6 +6,6 @@ const calculator = (() => {
     return { add, sub, mul, div, };
 })();
 
-calculator.add(3, 5); // 8
-calculator.sub(6, 2); // 4
-calculator.mul(14, 5534); // 77476
+console.log(calculator.add(3, 5)); // 8
+console.log(calculator.sub(6, 2)); // 4
+console.log(calculator.mul(14, 5534)); // 77476
